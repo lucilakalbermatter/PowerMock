9802ca978c91032d195d388f4fbd880726ab1080
lucilakalbermatter <lucilakalbermatter@hotmail.com>
2021-12-14 15:40:12 +0100

    second commit

--- src/test/java/UtilityTest.java
+++ src/test/java/UtilityTest.java
@@ -1,13 +1,13 @@
 package test.java;
 
 import static org.junit.Assert.assertEquals;
-
 import main.java.Utility;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(Utility.class)
